f93486c4308dc17a430e3aa97bafccd8ca5cfed0
David Roundy <roundyd@physics.oregonstate.edu>
2019-07-01 06:44:31 -0700

    First draft of ACME handling for warp
    This draft creates a macro because I can't figure out how to make the
    types work to create a function that does the same thing.  I am not
    happy with this approach, and hopefully will get some help from
    @seanmonstar.

---
+++ src/lib.rs
@@ -0,0 +1,66 @@
+/// Run forever on the current thread, serving using TLS to serve on the given domain.
+///
+/// Errors are reported on stderr.
+#[macro_export]
+macro_rules! lets_encrypt {
+    ($service:expr, $domain:expr) => {{
+        use acme_client::Directory;
+        use warp::{path, Filter};
+
+        let directory = Directory::lets_encrypt()
+            .expect("Trouble connecting to let's encrypt");
+        let account = directory.account_registration().register()
+            .expect("Trouble registring for an account.");
+
+        // Create a identifier authorization for example.com
+        let authorization = account.authorization($domain)
+            .expect("Trouble creating authorization for the account for the domain.");
+
+        // Validate ownership of example.com with http challenge
+        let http_challenge = authorization.get_http_challenge()
+        // .ok_or("HTTP challenge not found")
+            .expect("Problem with the challenge");
+
+        let authorization = http_challenge.key_authorization().to_string();
+        let token_name = Box::leak(http_challenge.token().to_string().into_boxed_str());
+        std::thread::spawn(move || {
+            let token = warp::path!(".well-known" / "acme-challenge")
+                .and(warp::path(token_name))
+                .map(move || {
+                    authorization.clone()
+                });
+            // let redirect = warp::redirect::redirect();
+            warp::serve(token
+                        // .or(redirect)
+            )
+                .run(([0, 0, 0, 0], 80));
+        });
+
+        // http_challenge.save_key_authorization("/var/www")?;
+        std::thread::sleep(std::time::Duration::from_millis(500));
+        http_challenge.validate().expect("Trouble validating.");
+
+        let cert = account.certificate_signer(&[$domain]).sign_certificate()
+            .expect("Trouble signing?");
+        cert.save_signed_certificate("certificate.pem").expect("Touble saving pem");
+        cert.save_private_key("certificate.key").expect("Trouble saving key");
+
+        warp::serve($service)
+            .tls("certificate.pem", "certificate.key")
+            .run(([0, 0, 0, 0], 443));
+    }}
+}
+
+#[cfg(test)]
+mod tests {
+    fn should_compile_but_not_complete() {
+        use warp::Filter;
+        let x = warp::path("foo")
+            .map(|| { "bar" });
+        lets_encrypt!(x, "example.com");
+    }
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
